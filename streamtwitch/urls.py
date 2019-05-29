b13f755db0c6be4a2d583c1be1f41fd0d1efc2d2
Juan Verhook <juanverhook@Juans-Air.home>
2019-05-29 03:00:34 +0200

    Task-5 Follow/Unfollow

--- streamtwitch/urls.py
+++ streamtwitch/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('logout', views.logout, name="logout"),
     path('search', views.search, name="search"),
     path('get_followers', views.get_followers, name='get_followers'),
-    #path('follow_user', views.follow_user, name='follow_user'),
+    path('unfollow_user/<user_id>/', views.unfollow_user, name='unfollow_user'),
     path('follow_user/<user_id>/', views.follow_user, name="follow_user"),
 
     #url('^purchases/(?P<username>.+)/$', PurchaseList.as_view()),
--- streamtwitch/views.py
+++ streamtwitch/views.py
@@ -6,6 +6,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from social_core.backends.twitch import TwitchOAuth2
 from twitch import TwitchClient
+from .models import Event
 
 
 # Create your views here.
@@ -17,6 +18,7 @@ def home(request):
     user = request.user
     social = user.social_auth.get(provider='twitch')
     client = TwitchClient(client_id='9hfygng7md3x7maw2g4uko0ednm3hk', oauth_token=social.extra_data['access_token'])
+    get_followers(request)
     return render(request, 'home.html')
 
 @api_view(['GET'])
@@ -35,7 +37,7 @@ def search(request):
         for channel in channels:
     	    levenshtein_distance = distance(search_query, channel.display_name)
     	    if levenshtein_distance in sorting_dict:
-    		    sorting_dict[levenshtein_distance] =  sorting_dict[levenshtein_distance] + [channel.display_name]#.append(channel.display_name)
+    		    sorting_dict[levenshtein_distance] =  sorting_dict[levenshtein_distance] + [channel.display_name]
     	    else:
     		    sorting_dict[levenshtein_distance] = [channel.display_name]
         users_list = []
@@ -50,35 +52,76 @@ def search(request):
 @permission_classes((IsAuthenticated,))
 def get_followers(request):
     """
-    This function returns a json with the ids of all the followers of our user
+    This function creates an event for all the followed channels of our user
+    It returns a json with the information of all the followers of our user
     """
     user = request.user
     social = user.social_auth.get(provider='twitch')
     client = TwitchClient(client_id='9hfygng7md3x7maw2g4uko0ednm3hk', oauth_token=social.extra_data['access_token'])
-    return JsonResponse({'follows': client.users.get_follows(social.uid)})
+    follows = client.users.get_follows(social.uid)
+    for follower in follows:
+        previously_followed = Event.objects.filter(user=user, streamer_id=follower['channel'].id, event_type="Followed user")
+        if not previously_followed:
+            Event.objects.create(user=user, streamer_id=follower['channel'].id, event_type="Followed user")
+    return JsonResponse({'follows': follows})
 
 
 @api_view(['GET'])
 @permission_classes((IsAuthenticated,))
 def follow_user(request, user_id):
     """
-    This function follows a user
+    This function follows a streamer as long as the last follow event is older
+    than the last unfollow event.
     """
     user = request.user
     social = user.social_auth.get(provider='twitch')
     client = TwitchClient(client_id='9hfygng7md3x7maw2g4uko0ednm3hk', oauth_token=social.extra_data['access_token'])
-    return JsonResponse({'follows': client.users.follow_channel(social.uid, user_id)})
+    last_follow = Event.objects.filter(user=user, streamer_id=user_id, event_type="Followed user").order_by('-id').first()
+    last_unfollow = Event.objects.filter(user=user, streamer_id=user_id, event_type="Unfollowed user").order_by('-id').first()
+
+
+    if last_follow:
+        if last_unfollow:
+            time1 = last_follow._meta.get_field('created_at').value_from_object(last_follow)
+            time2 = last_unfollow._meta.get_field('created_at').value_from_object(last_unfollow)
+            if (time1-time2).total_seconds() > 0:
+            	return(JsonResponse({"follows": "User was already following this channel"}))
+            else:
+                Event.objects.create(user=user, streamer_id=user_id, event_type="Followed user")
+                return(JsonResponse({'follows': client.users.follow_channel(social.uid, user_id)}))
+        return(JsonResponse({"follows": "User was already following this channel"}))
+    Event.objects.create(user=user, streamer_id=user_id, event_type="Followed user")
+    return(JsonResponse({'follows': client.users.follow_channel(social.uid, user_id)}))
 
 @api_view(['GET'])
 @permission_classes((IsAuthenticated,))
 def unfollow_user(request, user_id):
     """
-    This function unfollows a user
+    This function unfollows a user as long as the last follow event is more recent than the last
+    unfollow event.
     """
     user = request.user
     social = user.social_auth.get(provider='twitch')
     client = TwitchClient(client_id='9hfygng7md3x7maw2g4uko0ednm3hk', oauth_token=social.extra_data['access_token'])
-    return JsonResponse({'follows': client.users.unfollow_channel(social.uid, user_id)})  
+    last_follow = Event.objects.filter(user=user, streamer_id=user_id, event_type="Followed user").order_by('-id').first()
+    last_unfollow = Event.objects.filter(user=user, streamer_id=user_id, event_type="Unfollowed user").order_by('-id').first()
+
+    if not last_follow:
+        return(JsonResponse({"response:" "User was not following this channel"}))
+
+    if last_follow:
+        if last_unfollow:
+            time1 = last_follow._meta.get_field('created_at').value_from_object(last_follow)
+            time2 = last_unfollow._meta.get_field('created_at').value_from_object(last_unfollow)
+            if (time1-time2).total_seconds() > 0:
+                Event.objects.create(user=user, streamer_id=user_id, event_type="Unfollowed user")
+                client.users.unfollow_channel(social.uid, user_id)
+                return(JsonResponse({"response": "User succesfully unfollowed"}))
+            else:
+                return(JsonResponse({"response": "User was not following this channel"}))
+        Event.objects.create(user=user, streamer_id=user_id, event_type="Unfollowed user")
+        client.users.unfollow_channel(social.uid, user_id)
+        return JsonResponse({'response': "User succesfully unfollowed"})  
 
 def logout(request):
     """This function logs out an authenticated user"""
